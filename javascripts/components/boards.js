dece8f63ce5b2a60959012e061962c0cdd9c02f1
Matthew Jackson <mattshiloh@gmail.com>
2018-10-22 21:31:19 -0500

    sorta working

--- javascripts/components/boards.js
+++ javascripts/components/boards.js
@@ -1,22 +1,25 @@
 import { loadBoards } from '../data/boardsData.js';
+import { initializePinView } from '../components/pins.js';
 
 const bindEvents = () => {
     $('#user-boards').on('click', '.board-card', (e) => {
         const clickedBoardId = $(e.target).closest('.board-card').attr('id');
         $('#boards-page').hide();
         $('#pins-page').show();
+        initializePinView(clickedBoardId);
     })
 }
 
 const writeBoards = (boards) => {
     let domString = '';
     boards.forEach(board => {
-        domString += `
+        const boardImg = board.pins[0] ? board.pins[0].img_url : './db/default-img.jpeg';
+         domString += `
         <div id='${board.id}' class="board-card p-2">
           <img class="card-img-top" src="./db/default-img.jpeg" alt="Card image cap">
           <div class="card-body">
             <h5 class="card-title">${board.name}</h5>
-            <p class="card-text">42 Pins</p>
+            <p class="card-text">${board.pins.lenght} Pins</p>
           </div>
       </div>
       `
@@ -26,7 +29,9 @@ const writeBoards = (boards) => {
 
 const initializeBoardView = () => {
     loadBoards().then((boards) => {
-        writeBoards(boards);
+        return loadPinsOnBoards(boards);
+    }).then((boardsWithPins) => {
+        writeBoards(boardsWithPin);
         bindEvents();
     }).catch((error) => {
     })
